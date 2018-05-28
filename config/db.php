2f89fc8ed67fec496e7e2d20c5be4dfb77f920fb
Alex Villa <alex.villa.v@gmail.com>
2018-05-28 12:49:50 -0400

    Changed values for table and password

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=vendedores_insert',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=evtManager',
     'username' => 'root',
-    'password' => '',
+    'password' => 'colseco',
     'charset' => 'utf8',
 ];
